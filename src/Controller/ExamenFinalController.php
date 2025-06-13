4dc5d453bb947385d668d98cf8a3bf957e603d1c
Grego <106344297+G4Gs@users.noreply.github.com>
2025-06-12 21:19:09 -0300

    4

--- src/Controller/ExamenFinalController.php
+++ src/Controller/ExamenFinalController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+//obliga a que no envia a la pagina de error de synfony
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
 
 #[Route('/examen/final')]
 class ExamenFinalController extends AbstractController
@@ -66,13 +68,18 @@ class ExamenFinalController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_examen_final_delete', methods: ['POST'])]
-    public function delete(Request $request, ExamenFinal $examenFinal, ExamenFinalRepository $examenFinalRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$examenFinal->getId(), $request->request->get('_token'))) {
+   #[Route('/{id}', name: 'app_examen_final_delete', methods: ['POST'])]
+public function delete(Request $request, ExamenFinal $examenFinal, ExamenFinalRepository $examenFinalRepository): Response
+{
+    if ($this->isCsrfTokenValid('delete'.$examenFinal->getId(), $request->request->get('_token'))) {
+        try {
             $examenFinalRepository->remove($examenFinal, true);
+        } catch (ForeignKeyConstraintViolationException $e) {
+            $this->addFlash('error', 'No puedes eliminar el examen final porque tiene alumnos asociados. Elimina primero los registros de alumnos vinculados a este examen.');
+            return $this->redirectToRoute('app_examen_final_edit', ['id' => $examenFinal->getId()]);
         }
-
-        return $this->redirectToRoute('app_examen_final_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    return $this->redirectToRoute('app_examen_final_index', [], Response::HTTP_SEE_OTHER);
+}
 }
