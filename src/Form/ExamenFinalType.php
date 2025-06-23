b06bb2eb6f92636c7341868aa9d352e078fc89ab
Grego <106344297+G4Gs@users.noreply.github.com>
2025-06-22 21:42:31 -0300

    6.5

--- src/Form/ExamenFinalType.php
+++ src/Form/ExamenFinalType.php
@@ -10,7 +10,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-<<<<<<< HEAD
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ExamenFinalType extends AbstractType
@@ -62,47 +61,4 @@ class ExamenFinalType extends AbstractType
             'data_class' => ExamenFinal::class,
         ]);
     }
-=======
-use App\Entity\Docente;
-use App\Entity\Asignatura;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
-class ExamenFinalType extends AbstractType
-{
-  public function buildForm(FormBuilderInterface $builder, array $options): void
-  {
-    $builder
-        ->add('fecha')
-        ->add('presidente_id', EntityType::class, [
-            'class' => Docente::class,
-            'choice_label' => 'persona',
-            'label' => 'Presidente',
-        ])
-        ->add('Vocal1_id', EntityType::class, [
-            'class' => Docente::class,
-            'choice_label' => 'persona',
-            'label' => 'Vocal 1',
-            'required' => false,
-        ])
-        ->add('Vocal2_id', EntityType::class, [
-            'class' => Docente::class,
-            'choice_label' => 'persona',
-            'label' => 'Vocal 2',
-            'required' => false,
-        ])
-        ->add('asignatura_id', EntityType::class, [
-            'class' => Asignatura::class,
-            'choice_label' => 'nombre',
-            'label' => 'Asignatura',
-        ])
-    ;
-  }
-
-  public function configureOptions(OptionsResolver $resolver): void
-  {
-      $resolver->setDefaults([
-          'data_class' => ExamenFinal::class,
-      ]);
-  }
->>>>>>> 14397690d78e5c4d99a6bfbe6f9721f530690f30
 }
