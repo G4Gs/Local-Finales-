966a34405a9cb0f511483094d76b0fded9905dbd
JuanSistemasBasualdo <juan93basualdo@gmail.com>
2025-06-24 10:10:10 -0300

    juany

--- var/cache/dev/twig/4c/4c9e5ccd919dd5b06705576ec9c7fa51.php
+++ var/cache/dev/twig/4c/4c9e5ccd919dd5b06705576ec9c7fa51.php
@@ -97,9 +97,9 @@ class __TwigTemplate_5953a398d50aea81ed8b39631ba12bff extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
         // line 6
-        yield "    <h1>Listado de Exámenes de Alumnos</h1>
+        yield "    <h1 class=\"datos-filtered\">Listado de Exámenes de Alumnos</h1>
 
-    <table class=\"table\">
+    <table class=\"tabla_home\">
         <thead>
             <tr>
                 <th>ID</th>
@@ -208,9 +208,9 @@ class __TwigTemplate_5953a398d50aea81ed8b39631ba12bff extends Template
 {% block title %}ExamenAlumno index{% endblock %}
 
 {% block body %}
-    <h1>Listado de Exámenes de Alumnos</h1>
+    <h1 class=\"datos-filtered\">Listado de Exámenes de Alumnos</h1>
 
-    <table class=\"table\">
+    <table class=\"tabla_home\">
         <thead>
             <tr>
                 <th>ID</th>
