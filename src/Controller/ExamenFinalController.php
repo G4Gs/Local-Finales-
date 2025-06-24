9c88a4f4ebf28054f0ca46cdfe554fc9b6d43131
Grego <106344297+G4Gs@users.noreply.github.com>
2025-06-23 22:05:59 -0300

    0

--- src/Controller/ExamenFinalController.php
+++ src/Controller/ExamenFinalController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\ExamenAlumnoRepository;
 //obliga a que no envie a la pagina de error de synfony
 use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
